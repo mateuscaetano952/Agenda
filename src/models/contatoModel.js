722e697a252a237ff4efc6f45bb52418f3190816
Mateus Caetano <mateuscaetano952@gmail.com>
2025-02-24 17:45:31 -0300

    Rota delete 100%

--- src/models/contatoModel.js
+++ src/models/contatoModel.js
@@ -19,49 +19,8 @@ class Contato {
         this.contato = null;
     }
 
-
-    async criarContato(){
-        this.valida()
-        
-        if (this.errors.length > 0) return; 
-        this.success.push('Contato salvo com sucesso');
-        this.contato = await ContatoModel.create(this.body);
-     
-    }
-
-    valida(){
-        this.cleanUp();
-
-        if(this.body.email && !validator.isEmail(this.body.email)){
-            this.errors.push("Email invalido")
-        }
-
-        //validar se nome 
-        if(!this.body.nome){
-            this.errors.push("Nome é um campo obrigatorio");
-        }
-
-
-        //Precisar ou de um email ou número de telefone
-        if(!this.body.email && !this.body.numero){
-            this.errors.push("Precisa de um email ou um número de telefone");
-        }
-
-        
-
-    }
-
-    async edit(id){
-        this.valida();
-        if (this.errors.length > 0) return; 
-        
-        this.contato = await ContatoModel.findByIdAndUpdate(id, this.body, {new: true});
-        this.success.push('Contato editado com sucesso');
-        
-
-    }
-
-     static async achaContato(id) {
+    //Funções estaticas
+    static async achaContato(id) {
         try {
             const contato = await ContatoModel.findOne({ _id: id });
             return contato;
@@ -80,13 +39,59 @@ class Contato {
 
     static async deleteContato(id){
         const contato  = await ContatoModel.findOneAndDelete( {_id: id});
+        if(!contato) return;
+        return;
     }
 
 
+    async criarContato(){
+        this.valida()
+        
+        if (this.errors.length > 0) return; 
+        this.success.push('Contato salvo com sucesso');
+        this.contato = await ContatoModel.create(this.body);
+        return;
+     
+    }
 
-    //Verifica se tem elementos que não são Strings
+    async edit(id){
+        this.valida();
+        if (this.errors.length > 0) return; 
+        if(!id && id != String) {
+            this.errors.push("Ocorreu um erro no processamento"); 
+            return;
+        }
+
+        this.contato = await ContatoModel.findByIdAndUpdate(id, this.body, {new: true});
+        this.success.push('Contato editado com sucesso');
+        
+
+    }
+
+
+    valida(){
+        this.cleanUp();
+
+        if(this.body.email && !validator.isEmail(this.body.email)){
+            this.errors.push("Email invalido")
+        }
+
+        if(!this.body.nome){
+            this.errors.push("Nome é um campo obrigatorio");
+        }
+
+        //Precisar ou de um email ou número de telefone
+        if(!this.body.email && !this.body.numero){
+            this.errors.push("Precisa de um email ou um número de telefone");
+        }
+
+        
+
+    }
+
+    //Higienização dos campo
     cleanUp(){
-        //Verificar se tem campos que não são strings
+       
         for(const key in this.body) {
             if(typeof this.body[key] !== 'string')
                 this.body[key] = '';
